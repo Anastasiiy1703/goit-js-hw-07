70bbabd75843dd13a8f2de25f1ef3441e2b53917
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-10-01 11:47:38 +0300

    commit1

---
+++ js/01-gallery.js
@@ -0,0 +1,15 @@
+ import { galleryItems } from './gallery-items.js';
+Change code below this line
+
+const container = document.querySelector(".gallery");
+
+const imagesAll = galleryItems.map((image) => `
+  <li>
+    <img src="${image.preview}" alt="${image.description}" />
+  </li>
+`).join('');
+
+container.insertAdjacentHTML('beforeend', imagesAll);
+container.classList.add("images");
+
+console.log(galleryItems);
