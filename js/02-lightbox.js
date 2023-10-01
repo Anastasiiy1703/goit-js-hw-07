d4a656b1050ac3f614026b440bcdee6bf5df55b8
Anastasiiy1703 <anastasiya.onoyko1990@gmail.com>
2023-10-01 20:30:36 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector(".gallery");
+function createMarkup(arr) {
+  return arr.map(({preview, original, description })=> `
+   <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+  `).join('');
+    
+}
+container.insertAdjacentHTML('afterbegin', createMarkup(galleryItems));
+
+container.addEventListener('click', handlarClick);
+ function handlarClick (event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+
+     let lightbox = new SimpleLightbox('.gallery__item a', {
+        captionType: 'attr',
+        captionPosition: 'bottom',
+         captionDelay: 250,
+         captionsData: `alt`
+    });
+   
+};
+ 
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
